257e88ba825e6a2ef0a9fb2d5a01318217890b86
Tosin <tosino@examroom.ai>
2024-02-07 11:31:51 +0100

    Created function calls to validate addresses

--- web3/contracts/Address.sol
+++ web3/contracts/Address.sol
@@ -1,4 +1,80 @@
-//SPDEX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: UNLICENSED
+
 pragma solidity ^0.8.9;
 
-library Address
+library Address {
+
+   function isContract(address account) internal view returns(bool){
+       return account.code.length > 0;
+      
+   }
+
+   function sendValue(address payable recipient, uint256 amount) internal {
+      require (address(this).balance >= amount, "Address: Insufficient balance");
+   
+     (bool success, ) = recipient.call{value: amount}("");
+     require(success, "Address: unable to send value, recipient may have reverted");
+   
+   }
+
+   function functionCall(address target, bytes memory data) internal returns (bytes memory) {
+      return functionCall(target, data, "Address: low level call failed");
+}
+
+// function functionCall(address target, bytes memory data, string memory errorMessage) internal returns (bytes memory) {
+//     (bool success, bytes memory returndata) = target.call(data);
+//     require(success, errorMessage);
+//     return returndata;
+// }
+  function functionCall(address target, bytes memory data, uint256 value)
+   internal returns (bytes memory){
+     return functionCallwithValue(target, data, value, "Address: low level call with value failed");
+   }  
+   
+
+   function functionCallwithValue( address target, bytes memory data, uint256 value)
+   internal returns (bytes memory){
+    require(address(this).balance >= value, "Address: insufficient balance for call");
+    require (isContract(target), "Address: call to non-contract");
+    (bool success, bytes memory returndata) = target.call{value: value};
+      return verifyCallResult(success, returndata, errorMessage);
+
+   }
+
+   function functionStaticCall(address target, bytes memory data, string memory errorMessage)
+   internal view returns (bytes memory)
+   {
+     require(isContract(target), "Address: static call to non-contract");
+      (bool success, bytes memory returndata) = target.staticcall(data);
+       return verifyCallResult(success, returndata, errorMessage);
+
+   }
+
+    function functionDelegateCall(address target, bytes memory data, string memory errorMessage)
+      internal returns(bytes memory){
+          require(isContract(target), "Address: delegate call to non-contract");
+           (bool success, bytes memory returndata) = target.delegatecall(data);
+            return verifyCallResult(success, returndata, errorMessage);
+      }
+
+
+   function verifyCallResult(bool success, bytes memory returndata, string memory errorMessage)
+     internal pure returns (bytes memory){
+         if(success){
+            return returndata;
+         }else{
+              if(returndata.length >0){
+                 assembly {
+                    let returndata_size := mload(returndata)
+                     revert(add(32, returndata), returndata_size)
+                 }
+
+
+                 }else{
+                     revert (errorMessage);
+                 }
+              }
+         }
+     }
+
+}
